5d83c66edf1b536ea75dade03a6cdb6e16ac904b
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 23:16:27 +0530

    fdf

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 	res.json({
-    'fulfillmentText':phoness,
+    'fulfillmentText':phone,
     'fulfillmentMessages':[{"text":{"text":[phone]}}],
     'source':'gmail'
   })
