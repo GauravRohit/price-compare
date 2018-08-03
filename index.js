11ae96df02cc34fcd327eb1ca103e64d00ff6ffe
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 13:39:34 +0530

    express

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var http = require("http");
 var request =  require('request');
+var express = require('express');
+var app = express();
+
+app.post('/webhook',function(req,res){
+res.setHeader('content-Type','application/json');
 var city = req.body.queryResult.parameters['geo-city'];
 var w = getWeather(city);
 let response = " ";
