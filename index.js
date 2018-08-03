2c4a78d978bc4289d0df479de9da22c1d8108147
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 13:19:02 +0530

    fdf

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-var https = require("https");
+var http = require("http");
 var request =  require('request');
+var body-parser = require('body-parser');
 var express = require('express');
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoder({extended: true}));
 var path = require("path");
-var server = require('https').createServer(app);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+app.post('/webhook',function(req,res){
+res.setHeader('content-Type','application/json');
+var city = req.body.queryResult.parameters['geo-city'];
+var w = getWeather(city);
+let response = " ";
+let responseObj =("fulfillmentText":response
+                  ,"fulfillmentMessage":[{"text":{"text":[w]}])
+                  ,"source":""
+})
+var apiKey = 'f6dacb03346d39ed9172cd49941688a3'
+var result
+function cd (err,response,body){
+  if(err){
+    console.log('error:',error);
+  }
+  var weather = Json.parse(body)
+  if(weather.message === 'city not found'){
+    result = 'Unable to get weather '+ weather.message;
+  }
+  else{
+    result = 'Right now its '+weather.main.temp+' degree with '+ weather.weather[0].description;
+  }
+}
 
-app.get('/', function(req, res  ) {
-   var wunder={} ;
-var options = {
-    hostname: 'price-api.datayuge.com',
-    port: 443,
-    path: '/api/v1/compare/search?product=oneplus%015t&api_key=4ECmIcYuCkqKCAfbYzkzeulI9Vgpwe53Qnc',
-    method: 'GET'
-};
-var request = https.request(options, function(res) {
-   var body='';
-    res.setEncoding('utf8');
-    res.on('data', function(chunk) {
-
-        body += chunk;
-
-    });
-
-    res.on('end', function() {
-
-         wunder=JSON.parse(body);
-        console.log(wunder);
-console.log("")
-    });
-});
-
-// handle failure
-request.on('error', function(e) {
-    throw e;
-});
-	res.json({
-         	success:true,
-        	result:wunder
-         });
- request.end();
-  });
-app.listen(3000);
-
+function getWeather(city){
+  result = undefined;
+  var url = `http://api.openweathermap.org/data/2.5/weather?=q${city}&units=imperial&appid=${apiKeuy}`;
+  console.log(url);
+  var req = request(url,cb);
+  while(result === undefined){
+    require('deasync').runLoopOnce();
+  }
+  return result;
+}
