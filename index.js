6d4ebcc178926e86daca59433d808433a45199ff
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 13:26:33 +0530

    new code

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ res.setHeader('content-Type','application/json');
 var city = req.body.queryResult.parameters['geo-city'];
 var w = getWeather(city);
 let response = " ";
-let responseObj =("fulfillmentText":response
-                  ,"fulfillmentMessage":[{"text":{"text":[w]}])
-                  ,"source":""
+let responseObj ={"fulfillmentText":response
+                  ,"fulfillmentMessage":[{"text":{"text":[w]}}]
+                  ,"source":""}
 })
 var apiKey = 'f6dacb03346d39ed9172cd49941688a3'
 var result
