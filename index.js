82b4061867bc1c07be07aaf1cb4e55008d0cf721
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 12:24:15 +0530

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var https = require("https");
+var request =  require('request');
+var express = require('express');
+var app = express();
+var path = require("path");
+var server = require('https').createServer(app);
+
+app.get('/', function(req, res  ) {
+   var wunder={} ;
 var options = {
     hostname: 'price-api.datayuge.com',
     port: 443,
-    path: '/api/v1/compare/search?product=iphone%207&api_key=4ECmIcYuCkqKCAfbYzkzeulI9Vgpwe53Qnc',
+    path: '/api/v1/compare/search?product=oneplus%015t&api_key=4ECmIcYuCkqKCAfbYzkzeulI9Vgpwe53Qnc',
     method: 'GET'
 };
-var req = https.request(options, function(res) {
-    var body = '';
+var request = https.request(options, function(res) {
+   var body='';
     res.setEncoding('utf8');
     res.on('data', function(chunk) {
 
@@ -16,20 +24,19 @@ var req = https.request(options, function(res) {
 
     res.on('end', function() {
 
-        var wunder=JSON.parse(body);
-        //console.log("__");
-        //console.log(wunder);
-        return ({
-          "Message":"Success"
-             })
-
-
+         wunder=JSON.parse(body);
+        console.log(wunder);
     });
-
 });
 
-req.on('error', function(e) {
+// handle failure
+request.on('error', function(e) {
     throw e;
 });
-
-req.end();
+	res.json({
+         	success:true,
+        	result:wunder
+         });
+ request.end();
+  });
+app.listen(3000);
