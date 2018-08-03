9b71abb013f97398584ae1649d2706a651e17ba9
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 13:49:56 +0530

    deleted IO

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var http = require("http");
 var request =  require('request');
 var express = require('express');
 var app = express();
-
 app.post('/webhook',function(req,res){
 res.setHeader('content-Type','application/json');
 var city = req.body.queryResult.parameters['geo-city'];
