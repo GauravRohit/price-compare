bd21494a2ccb188c96ad6f584216384db2f77e48
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 18:35:07 +0530

    test code

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-var http = require("http");
-var request =  require('request');
 var express = require('express');
 var app = express();
-app.post('/webhook',function(req,res){
-res.setHeader('content-Type','application/json');
-var city = req.body.queryResult.parameters['geo-city'];
-var w = getWeather(city);
-let response = " ";
-let responseObj ={"fulfillmentText":"hahahahahahah"
-                  ,"fulfillmentMessage":[{"text":{"text":["lolo"]}}]
-                  ,"source":""
-                }
-})
-var apiKey = 'f6dacb03346d39ed9172cd49941688a3'
-var result
-function cd (err,response,body){
-  if(err){
-    console.log('error:',error);
-  }
-  var weather = Json.parse(body)
-  if(weather.message === 'city not found'){
-    result = 'Unable to get weather '+ weather.message;
-  }
-  else{
-    result = 'Right now its '+weather.main.temp+' degree with '+ weather.weather[0].description;
-  }
-}
 
-function getWeather(city){
-  result = undefined;
-  var url = 'https://price-api.datayuge.com/api/v1/compare/search?product=oneplus%015t&api_key=4ECmIcYuCkqKCAfbYzkzeulI9Vgpwe53Qnc';
-  console.log(url);
-  var req = request(url,cb);
-  while(result === undefined){
-    require('deasync').runLoopOnce();
-  }
-  return result;
-}
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
