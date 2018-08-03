11351ce8d23b73f3f5fdb3fd26aff131e48bec2f
Shikher Walia <shikherwalia@gmail.com>
2018-08-03 18:36:57 +0530

    test code

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.json({
+    'message':'nyc'
+  })
 });
 
 app.listen(port, function() {
